05971869d494b21ae1131d82828b935b3ae609d9
Edvaldo Junior <EJUNIOR@volvocars.com>
2022-10-28 20:44:42 +0200

    Adding unit tests

---
+++ Tests/MoneyTests.cs
@@ -0,0 +1,96 @@
+﻿namespace Tests
+{
+    using Domain;
+    using FluentAssertions;
+    using Xunit;
+
+    public class MoneyTests
+    {
+        [Fact]
+        public void Sum_of_two_moneys_produces_correct_result()
+        {
+            // Arrange
+            var money1 = new Money(
+                oneCentCount: 1,
+                tenCentCount: 2,
+                quarterCount: 3,
+                oneEuroCount: 4,
+                fiveEuroCount: 5,
+                twentyEuroCount: 6);
+
+            var money2 = new Money(
+                oneCentCount: 1,
+                tenCentCount: 2,
+                quarterCount: 3,
+                oneEuroCount: 4,
+                fiveEuroCount: 5,
+                twentyEuroCount: 6);
+
+            // Act
+            var sum = money1 + money2;
+
+            // Assert
+            sum
+                .OneCentCount
+                .Should()
+                .Be(2);
+
+            sum.
+                TenCentCount
+                .Should()
+                .Be(4);
+
+            sum.
+                QuarterCount
+                .Should()
+                .Be(6);
+
+            sum.
+                OneEuroCount
+                .Should()
+                .Be(8);
+
+            sum.
+                FiveEuroCount
+                .Should()
+                .Be(10);
+
+            sum.
+                TwentyEuroCount
+                .Should()
+                .Be(12);
+        }
+
+        [Fact]
+        public void Two_money_instances_equal_if_contain_the_same_money_amount()
+        {
+            // Arrange
+            var money1 = new Money(
+                oneCentCount: 1,
+                tenCentCount: 2,
+                quarterCount: 3,
+                oneEuroCount: 4,
+                fiveEuroCount: 5,
+                twentyEuroCount: 6);
+
+            var money2 = new Money(
+                oneCentCount: 1,
+                tenCentCount: 2,
+                quarterCount: 3,
+                oneEuroCount: 4,
+                fiveEuroCount: 5,
+                twentyEuroCount: 6);
+
+            // Act
+
+            // Assert
+            money1
+                .Should()
+                .Be(money2);
+
+            money1
+                .Should()
+                .Be(money2.GetHashCode());
+        }
+    }
+}
--- service/src/Domain/Money.cs
+++ service/src/Domain/Money.cs
@@ -1,20 +1,18 @@
 ﻿namespace Domain;
 
-using Microsoft.VisualBasic.CompilerServices;
-
-public class Money
+public class Money : ValueObject<Money>
 {
     public Money(
         int oneCentCount,
         int tenCentCount,
-        int quarterCentCount,
+        int quarterCount,
         int oneEuroCount,
         int fiveEuroCount,
         int twentyEuroCount)
     {
         OneCentCount += oneCentCount;
         TenCentCount += tenCentCount;
-        QuarterCentCount += quarterCentCount;
+        QuarterCount += quarterCount;
         OneEuroCount += oneEuroCount;
         FiveEuroCount += fiveEuroCount;
         TwentyEuroCount += twentyEuroCount;
@@ -23,7 +21,7 @@ public class Money
     public int FiveEuroCount { get; private set; }
     public int OneCentCount { get; private set; }
     public int OneEuroCount { get; private set; }
-    public int QuarterCentCount { get; private set; }
+    public int QuarterCount { get; private set; }
     public int TenCentCount { get; private set; }
     public int TwentyEuroCount { get; private set; }
 
@@ -32,9 +30,34 @@ public class Money
         return new Money(
             oneCentCount: money1.OneCentCount + money2.OneCentCount,
             tenCentCount: money1.TenCentCount + money2.TenCentCount,
-            quarterCentCount: money1.QuarterCentCount + money2.QuarterCentCount,
+            quarterCount: money1.QuarterCount + money2.QuarterCount,
             oneEuroCount: money1.OneEuroCount + money2.OneEuroCount,
             fiveEuroCount: money1.FiveEuroCount + money2.FiveEuroCount,
             twentyEuroCount: money1.TwentyEuroCount + money2.TwentyEuroCount);
+    }
+
+    protected override bool EqualsCore(Money other)
+    {
+        return
+            OneCentCount == other.OneCentCount
+            && TenCentCount == other.TenCentCount
+            && QuarterCount == other.QuarterCount
+            && OneEuroCount == other.OneEuroCount
+            && FiveEuroCount == other.FiveEuroCount
+            && TwentyEuroCount == other.TwentyEuroCount;
+    }
+
+    protected override int GetHashCodeCore()
+    {
+        unchecked
+        {
+            int hashCode = OneCentCount;
+            hashCode = (hashCode * 397) ^ TenCentCount;
+            hashCode = (hashCode * 397) ^ QuarterCount;
+            hashCode = (hashCode * 397) ^ OneEuroCount;
+            hashCode = (hashCode * 397) ^ FiveEuroCount;
+            hashCode = (hashCode * 397) ^ TwentyEuroCount;
+            return hashCode;
+        }
     }
 }
