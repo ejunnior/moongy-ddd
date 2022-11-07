d7cef6e39c599ef96cf1b6393687fc6131c38da6
Edvaldo Junior <EJUNIOR@volvocars.com>
2022-11-07 20:58:42 +0100

    Database script

--- client/src/SnackMachineViewModel.cs
+++ client/src/SnackMachineViewModel.cs
@@ -7,6 +7,8 @@ public class SnackMachineViewModel : ViewModel
 {
     private readonly SnackMachine _snackMachine;
 
+    private string _menssage = string.Empty;
+
     public SnackMachineViewModel(SnackMachine snackMachine)
     {
         _snackMachine = snackMachine;
@@ -16,23 +18,65 @@ public class SnackMachineViewModel : ViewModel
         InsertEuroCommand = new Command(() => InsertMoney(Money.Euro));
         InsertFiveEuroCommand = new Command(() => InsertMoney(Money.FiveEuro));
         InsertTwentyEuroCommand = new Command(() => InsertMoney(Money.TwentyEuro));
+        ReturnMoneyCommand = new Command(() => ReturnMoney());
+        BuySnackCommand = new Command(() => BuySnack());
     }
 
+    public Command BuySnackCommand { get; private set; }
     public override string Caption => "Snack Machine";
     public Command InsertCentCommand { get; private set; }
+
     public Command InsertEuroCommand { get; private set; }
+
     public Command InsertFiveEuroCommand { get; private set; }
+
     public Command InsertQuarterCommand { get; private set; }
+
     public Command InsertTenCentCommand { get; private set; }
+
     public Command InsertTwentyEuroCommand { get; private set; }
 
-    public string MoneyInTransaction => _snackMachine.MoneyInTransaction.Amount.ToString();
+    public string Message
+    {
+        get
+        {
+            return _menssage;
+        }
+        set
+        {
+            _menssage = value;
+            Notify();
+        }
+    }
 
-    private void InsertMoney(Money money)
+    public Money MoneyInside => _snackMachine.MoneyInside + _snackMachine.MoneyInTransaction;
+    public string MoneyInTransaction => _snackMachine.MoneyInTransaction.ToString();
+    public Command ReturnMoneyCommand { get; private set; }
+
+    public void BuySnack()
+    {
+        _snackMachine.BuySnack();
+        NotifyClient("You have bought a snack");
+    }
+
+    public void ReturnMoney()
+    {
+        _snackMachine.ReturnMoney();
+        NotifyClient("Money was returned");
+    }
+
+    private void InsertMoney(Money coinOrNote)
     {
         _snackMachine
-            .InsertMoney(money);
+            .InsertMoney(coinOrNote);
 
-        Notify("MoneyInTransaction");
+        NotifyClient("You have inserted " + coinOrNote);
+    }
+
+    private void NotifyClient(string message)
+    {
+        Message = message;
+        Notify(nameof(MoneyInTransaction));
+        Notify(nameof(MoneyInside));
     }
 }
